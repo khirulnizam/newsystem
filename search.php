c751335c36f4bafac90f00ebd8dc676ff6984033
Khirulnizam Abd Rahman <khirulnizam@gmail.com>
2020-03-22 21:27:28 +0800

    Applying Bootstrap template

--- search.php
+++ search.php
@@ -1,38 +1,72 @@
 <?php
-//filename listing.php
+	include "header.template.php";
+	//getbootstrap.com
+?>
+<div class="float-right">
+	<form method="get" action=""
+		class="form-inline">
+		<input type="text" name="keyword" 
+		class="form-control"
+		placeholder="Enter student name here">
+		<button type="submit" class ="btn btn-primary">
+		Search</button>
+	</form>
+</div>
+<?php
+//filename search.php
 //use the previous settings
 include "dbconnect.php";
 
 //embed SQL commands
-$sql = "SELECT id, matrixno, name, address, dob 
-			FROM students";
+if(isset($_GET['keyword'])){//based on keyword entered
+	$keyword=$_GET['keyword'];
+	$sql = "SELECT id, matrixno, name, address, dob 
+				FROM students 
+				WHERE name LIKE '%$keyword%'";
+}
+else{//first time page load, list all
+	$sql = "SELECT id, matrixno, name, address, dob 
+				FROM students ";
+}
+
+
 //execute sql commands that will return result set
 $result = mysqli_query($conn, $sql);
 
 //check records fetched available
 if (mysqli_num_rows($result) > 0) {
-?>
-	<table>
-		<thead>
-			<td>ID</td>
-			<td>Matrix</td>
-			<td>Name</td>
-			<td>Address</td>
-		</thead>
-<?php
     // output data of each row
+    ?>
+	<table class="table table-striped">
+		<tr>
+			<th>ID</th>
+			<th>Matrix</th>
+			<th>Name</th>
+			<th>DOB</th>
+			<th>Address</th>
+		</tr>
+	<?php
     while($row = mysqli_fetch_assoc($result)) {
-        echo "Student ID ".$row['id']."<br>";
-        echo "Matrix :".$row['matrixno'] ."<br>";
-        echo "Name :".$row['name'] ."<br>";
-        echo "DOB :".$row['dob'] ."<br>";
-        echo "Address: ".$row['address']."<hr>\n";
+    	echo "<tr>";
+        echo "<td> ".$row['id']."</td>";
+        echo "<td> ".$row['matrixno'] ."</td>";
+        echo "<td> ".$row['name'] ."</td>";
+        echo "<td> ".$row['dob'] ."</td>";
+        echo "<td> ".$row['address']."</td>";
+        echo "</tr>\n";
     }
+    ?>
+	</table>
+    <?php
 } 
 else {
-    echo "No results";
+    echo "Search not found";
 }
 
 //purge dbconnect
 mysqli_close($conn);
+?>
+
+<?php
+	include "footer.template.php";
 ?>
